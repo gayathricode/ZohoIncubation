0031508457674fd4c105492c88bb1da97ec91819
gayathricode <93397855+gayathricode@users.noreply.github.com>
2021-10-29 22:05:45 +0530

    creation

---
+++ Programs/InchToMeter.java
@@ -0,0 +1,29 @@
+/*Write a Java program that reads a number in inches, converts it to meters. 
+
+Note: One inch is 0.0254 meters.
+
+Test Data
+
+Input a value for inch: 1000
+
+Expected Output :
+
+1000.0 inch is 25.4 meters.*/
+
+
+
+package com.company;
+
+import java.util.Scanner;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int value=1000;
+        float inch=0.0254f;
+        float meter =value*inch;
+        System.out.println("value of meter is "+meter);
+    }
+}
