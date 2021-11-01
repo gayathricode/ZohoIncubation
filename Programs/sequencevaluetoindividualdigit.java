abcac78aefe12bc8d999b6b628c6684ad5a1d416
gayathricode <93397855+gayathricode@users.noreply.github.com>
2021-11-01 22:30:36 +0530

    Update sequencevaluetoindividualdigit.java

--- Programs/sequencevaluetoindividualdigit.java
+++ Programs/sequencevaluetoindividualdigit.java
@@ -8,29 +8,6 @@ Expected Output :
 
 1 2 3 4 5 6
   
-  package com.company;
-
-
-import java.util.Collection;
-import java.util.Scanner;
-
-public class Main
-{
-
-    public static void main(String[] args)
-    {
-        int ii=123456;
-        String s=Integer.toString(ii);
-        System.out.println(ii);
-        char[] ch = new char[s.length()];
-        for (int i = 0; i < s.length(); i++) {
-            ch[i] = s.charAt(i);
-        }
-        for (char c : ch) {
-            System.out.println(c+" ");
-        }
-    }
-
 
 
 }
